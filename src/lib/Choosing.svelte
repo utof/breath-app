78025487e8b609b999db199c87e607db02fd63d1
utof <bondar030@mail.ru>
2025-04-10 15:30:58 +0300

    refactor state mgmt and rename button to choosing

---
+++ src/lib/Choosing.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let params: Record<string, number>;
+	function handleInput(event: Event, key: string) {
+		const input = event.target as HTMLInputElement;
+		params[key] = parseInt(input.value) || 0; // Fallback to 0 if input is invalid
+	}
+</script>
+
+<div class="flex flex-col gap-4">
+	<div class="bg-blue-100">
+		{#each Object.entries(params) as [key, value]}
+			<div class="mb-6">
+				<span class="mx-8">{key}</span>
+				<input
+					type="number"
+					{value}
+					on:input={(e) => handleInput(e, key)}
+					class="w-16 rounded border px-2 text-center"
+				/>
+			</div>
+		{/each}
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,32 @@
 <script lang="ts">
-	import Button from '../lib/Button.svelte';
+	import Choosing from '../lib/Choosing.svelte';
 	import Breath from '../lib/Breath.svelte';
-	const texts: any[] = $state([
-		{ name: 'breaths', amount: '30' },
-		{ name: 'retention', amount: '1.50' }
-	]);
+	const statevals = {
+		CHOOSING: 'choosing',
+		BREATHING: 'breathing',
+		FINISHED: 'finished'
+	};
+	let current_state = $state(statevals.CHOOSING);
+
+	const breath_params: Record<string, number> = $state({
+		breaths: 30,
+		sheesh: 1.3
+	});
 
 	function showBreath(): void {
 		console.log('a');
+		current_state = statevals.CHOOSING;
 	}
 </script>
 
 <div class="flex h-screen items-center justify-center bg-amber-100">
 	<div class="max-w-md min-w-[200px] bg-red-300 text-center">
-		<h1 class="mb-4 text-4xl">Welcome</h1>
-		{#each texts as text}
-			<Button name={text.name} value={text.amount} />
-		{/each}
-		<Breath params={texts} onClick={() => showBreath()} />
+		<h1 class="mb-10 text-4xl">Welcome</h1>
+		{#if current_state === statevals.CHOOSING}
+			<Choosing params={breath_params} />
+		{:else}
+			<h1>hi</h1>
+		{/if}
+		<Breath params={breath_params} onClick={() => showBreath()} />
 	</div>
 </div>
